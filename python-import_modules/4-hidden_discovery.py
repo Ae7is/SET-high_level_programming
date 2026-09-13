ef3cf58b9c104da7da3d740c2084bda7ee66d29c
Ae7is <aejis777@gmail.com>
2026-09-13 07:41:35 +0100

    This program prints all the names defined by the compiled module hidden_4.pyc

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Print names defined by a compiled module."""
+
+import hidden_4
+
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
