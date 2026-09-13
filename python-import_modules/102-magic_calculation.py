266e1c8aa1605e36bcb8fc2bce9695abc1d2933a
Ae7is <aejis777@gmail.com>
2026-09-13 08:29:59 +0100

    This function does exactly the same as the following Python bytecode

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Do a magic calculation."""
+
+
+def magic_calculation(a, b):
+    """Perform the calculation shown by the bytecode."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
